8dc5b86b5119f1e4f080ad5cf177192569b2dae7
maikiencuong <maikiencuongiuh@gmail.com>
2021-09-14 21:56:12 +0700

    [repo] tao repository va class Test

---
+++ src/main/java/maikiencuong/datajpa/student/repository/StudentRepository.java
@@ -0,0 +1,16 @@
+package maikiencuong.datajpa.student.repository;
+
+import maikiencuong.datajpa.student.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Student> findAllByLastNameContainingIgnoreCase(String lastName);
+
+    List<Student> findAllByMalopContainingIgnoreCase(String malop);
+    
+}
--- src/test/java/maikiencuong/datajpa/student/StudentDataJpaApplicationTests.java
+++ src/test/java/maikiencuong/datajpa/student/StudentDataJpaApplicationTests.java
@@ -1,13 +1,93 @@
 package maikiencuong.datajpa.student;
 
+import maikiencuong.datajpa.student.entity.Student;
+import maikiencuong.datajpa.student.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootTest
 class StudentDataJpaApplicationTests {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
+    //    @Test
+    void insertStudent() {
+        studentRepository.save(Student.builder()
+                .firstName("Mai Kiên")
+                .lastName("Cường")
+                .dateOfBirth(LocalDate.now())
+                .malop("DHKTPM14")
+                .build());
+
+        studentRepository.save(Student.builder()
+                .firstName("Mai Kiên")
+                .lastName("Huy")
+                .dateOfBirth(LocalDate.now())
+                .malop("DHKTPM14")
+                .build());
+
+        studentRepository.save(Student.builder()
+                .firstName("Mai Kiên")
+                .lastName("Dương")
+                .dateOfBirth(LocalDate.now())
+                .malop("DHKTPM14")
+                .build());
+
+        studentRepository.save(Student.builder()
+                .firstName("Mai Kiên")
+                .lastName("Hoàng")
+                .dateOfBirth(LocalDate.now())
+                .malop("DHKTPM14")
+                .build());
+    }
+
     @Test
-    void contextLoads() {
+    void findAll() {
+        System.out.println("-------------find all");
+        for (Student s : studentRepository.findAll()) {
+            System.out.println("student = " + s);
+            System.out.println("-------------");
+        }
+    }
+
+    @Test
+    void findAllByMalop() {
+        System.out.println("-------------find all by malop");
+        for (Student s : studentRepository.findAllByMalopContainingIgnoreCase("dhktpm14")) {
+            System.out.println("student = " + s);
+            System.out.println("-------------");
+        }
+    }
+
+    @Test
+    void findAllByLastName() {
+        System.out.println("-------------find all by lastName");
+        for (Student s : studentRepository.findAllByLastNameContainingIgnoreCase("cường")) {
+            System.out.println("student = " + s);
+            System.out.println("-------------");
+        }
+    }
+
+    @Test
+    void update() {
+        List<Student> dhktpm14 = studentRepository.findAllByMalopContainingIgnoreCase("dhktpm14");
+        for (Student s : dhktpm14) {
+            s.setMalop("DHKTPM15");
+            studentRepository.save(s);
+        }
+    }
+
+    @Test
+    void delete() {
+        List<Student> dhktpm14 = studentRepository.findAllByMalopContainingIgnoreCase("dhktpm15");
+        for (Student s : dhktpm14) {
+            studentRepository.delete(s);
+        }
     }
 
 }
